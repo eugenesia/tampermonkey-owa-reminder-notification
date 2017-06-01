589e52a904c7f351262dc40631052349b150cf00
Eugene Sia <eugene@eugenesia.co.uk>
2017-06-01 16:03:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Tampermonkey OWA Reminder Notification
+======================================
+
+This is a Tampermonkey script to output notifications on Outlook Web Access
+reminders, such as reminders for meetings.
+
+When using Outlook Web Access in the browser, meeting reminders do not popup
+and are easily missed.
+
+This checks for new reminders and outputs Growl-like notifications when they
+appear on the OWA tab.
+
---
+++ user.js
@@ -0,0 +1,86 @@
+// ==UserScript==
+// @name         OWA reminder notification
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  Popup reminders from Outlook OWA.
+// @match        https://outlook.office.com/owa/*
+// @match        https://outlook.office365.com/owa/*
+// @copyright    2017+, Eugene Sia
+// @grant        GM_notification
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+
+  var lastReminderCount = 0;
+
+  // Check for updates every few seconds.
+  setInterval(checkReminderChange, 15000);
+
+
+  // Main function checking for new reminders.
+  function checkReminderChange() {
+
+    var reminderCount = countReminders();
+
+    // Number of reminders increased, there is a new reminder!
+    if (reminderCount > lastReminderCount) {
+      var latestReminder = getLatestReminder();
+
+      var popupText = (latestReminder.isOverdue ? 'Overdue' : 'In ' + latestReminder.timeToStart) + '\n' +
+        latestReminder.timeDuration + '\n' +
+        latestReminder.location;
+
+      GM_notification(popupText, 'Reminder: ' + latestReminder.title);
+    }
+
+    // There was a change in the count, record the new count so
+    // we can detect further changes.
+    if (reminderCount !== lastReminderCount) {
+      lastReminderCount = reminderCount;
+    }
+  }
+
+
+  // Count the number of notification popups.
+  function countReminders() {
+    return document.querySelectorAll('.o365cs-notifications-notificationPopup .o365cs-notifications-reminders-flexpaneitem').length;
+  }
+
+
+  // Get the latest reminder and return data about the reminder.
+  function getLatestReminder() {
+
+    var reminders = document.querySelectorAll('.o365cs-notifications-notificationPopup .o365cs-notifications-reminders-flexpaneitem');
+    var latestReminder = reminders[0];
+
+    // Get fields for the reminder.
+    var title = latestReminder.querySelector('.o365cs-notifications-reminders-title').innerText;
+
+    // Countdown to event start e.g. '11 Min'.
+    var timeToStartValue = latestReminder.querySelector('.o365cs-notifications-toastReminders-timeToStartValue').innerText;
+    var timeToStartUnit = latestReminder.querySelector('.o365cs-notifications-reminders-timeToStartUnit').innerText;
+
+    // Time duration e.g. '15:00 - 15:30'.
+    var timeDuration = latestReminder.querySelector('.o365cs-notifications-reminders-timeDuration').innerText;
+
+    var location = latestReminder.querySelector('.o365cs-notifications-reminders-location').innerText;
+
+    // If "Overdue" text is shown, then the event is overdue.
+    var isOverdue = true;
+    var overdueLabel = latestReminder.querySelector('.o365cs-notifications-toastReminders-overdue');
+    if (overdueLabel.style.display !== undefined && overdueLabel.style.display === 'none') {
+      isOverdue = false;
+    }
+
+    return {
+      title: title,
+      timeToStart: timeToStartValue + ' ' + timeToStartUnit,
+      timeDuration: timeDuration,
+      location: location,
+      isOverdue: isOverdue,
+    };
+  }
+})();
