288761d8d24d09389b91971def5d04e4e6e51757
Eugene Sia <eugene@eugenesia.co.uk>
2018-02-03 20:19:26 +0000

    Add "Expand" button

--- musicnotes_unpreview/user.js
+++ musicnotes_unpreview/user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Musicnotes unpreview
 // @namespace    http://eugenesia.net/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Remove preview overlay from sheet music.
 // @author       Eugene Sia <eugene@eugenesia.co.uk>
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
@@ -51,6 +51,20 @@ var inline_src = (<><![CDATA[
       // Redraw notes.
       geval(scriptTxt);
     });
+
+  // Make song window larger for screen capture or print.
+  function expandSong() {
+    let songDiv = document.querySelector('[class*=col-lg-5]');
+    songDiv.className = songDiv.className.replace('col-lg-5', 'col-lg-12');
+  }
+
+  let expandBtn = document.createElement('button');
+  let btnText = document.createTextNode('Expand');
+  expandBtn.appendChild(btnText);
+
+  document.querySelector('.controls').appendChild(expandBtn);
+
+  expandBtn.addEventListener('click', expandSong);
 
 /* jshint ignore:start */
 ]]></>).toString();
